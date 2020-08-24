5f3e8cb7a0e582c2e71f1c51171ca841987c40c8
Eduardo Rocha <eduardo.rocha.2001@gmail.com>
2020-08-24 09:45:23 -0300

    commit de teste no vscode

--- Exercicios/dezenas.py
+++ Exercicios/dezenas.py
@@ -1,8 +1,8 @@
 '''Faça um programa em Python que recebe um número inteiro e imprime seu dígito das dezenas. Observe o exemplo abaixo:
 Exemplo 1:
-Entrada de Dados:
+Entrada de Dados =>
 Digite um número inteiro: 78615
-Saída de Dados:
+Saída de Dados =>
 O dígito das dezenas é 1'''
 
 numero = int(input('Digite um número inteiro: '))
